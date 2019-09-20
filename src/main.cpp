a841abb4a43dda4178a9798eec5cc595c5315796
yiqiaowang <yi.qiao.wang@intact.net>
2019-09-20 13:36:28 -0400

    add dbg-macro to base example

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include "spdlog/spdlog.h"
+#include "dbg.h"
 
 int main() 
 {
     spdlog::info("Welcome to spdlog!");
+
+    std::string message = "Hello World";
+    dbg(message);
+
+    return 0;
 }
